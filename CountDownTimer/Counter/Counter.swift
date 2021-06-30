3b8a7a093a400080eaf2dbf45a991328bf2d5597
sparrow <seekfunbook@163.com>
2021-06-30 09:11:01 +0800

    3

---
+++ CountDownTimer/Counter/Counter.swift
@@ -0,0 +1,28 @@
+//
+//  Counter.swift
+//  Counter
+//
+//  Created by sparrow on 2021/6/30.
+//
+
+import Foundation
+
+struct Counter {
+    static func getCountDownTime(_ start: String) -> Double {
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat="yyyy-MM-dd HH:mm:ss"
+        let endDate = dateFormatter.date(from: start)
+        
+        guard let endDate1 = endDate else {
+            return 0
+        }
+        
+        let endTimestamp = endDate1.timeIntervalSince1970
+        let currentTimestamp = Date().timeIntervalSince1970
+        if endTimestamp < currentTimestamp {
+            return 0
+        }
+        return endTimestamp - currentTimestamp
+    }
+}
